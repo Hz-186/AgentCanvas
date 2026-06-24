989b371cc8cdc0550a894fd9a76efae0809e11b2
Hz-186 <zhanghongze186@gmail.com>
2026-06-24 08:27:27 +0800

    feat: update dialog

---
+++ internal/interface/http/handler/dialog_handler.go
@@ -0,0 +1,109 @@
+package handler
+
+import (
+	"net/http"
+
+	dialogusecase "agentcanvas/internal/application/dialog_usecase"
+	agenterrors "agentcanvas/internal/pkg/errors"
+	"agentcanvas/internal/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+type DialogHandler struct {
+	service *dialogusecase.Service
+}
+
+func NewDialogHandler(service *dialogusecase.Service) *DialogHandler {
+	return &DialogHandler{service: service}
+}
+
+func (h *DialogHandler) List(c *gin.Context) {
+	ownerID, ok := currentUserID(c)
+	if !ok {
+		response.Error(c, http.StatusUnauthorized, agenterrors.CodeUnauthorized, agenterrors.ErrUnauthorized.Error())
+		return
+	}
+	items, err := h.service.List(c.Request.Context(), ownerID)
+	if err != nil {
+		writeAppError(c, err)
+		return
+	}
+	response.OK(c, items)
+}
+
+func (h *DialogHandler) Create(c *gin.Context) {
+	ownerID, ok := currentUserID(c)
+	if !ok {
+		response.Error(c, http.StatusUnauthorized, agenterrors.CodeUnauthorized, agenterrors.ErrUnauthorized.Error())
+		return
+	}
+	var req dialogusecase.CreateDialogRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		writeAppError(c, err)
+		return
+	}
+	item, err := h.service.Create(c.Request.Context(), ownerID, req)
+	if err != nil {
+		writeAppError(c, err)
+		return
+	}
+	response.OK(c, item)
+}
+
+func (h *DialogHandler) Get(c *gin.Context) {
+	ownerID, id, ok := h.ownerAndID(c)
+	if !ok {
+		return
+	}
+	item, err := h.service.Get(c.Request.Context(), ownerID, id)
+	if err != nil {
+		writeAppError(c, err)
+		return
+	}
+	response.OK(c, item)
+}
+
+func (h *DialogHandler) Update(c *gin.Context) {
+	ownerID, id, ok := h.ownerAndID(c)
+	if !ok {
+		return
+	}
+	var req dialogusecase.UpdateDialogRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		writeAppError(c, err)
+		return
+	}
+	item, err := h.service.Update(c.Request.Context(), ownerID, id, req)
+	if err != nil {
+		writeAppError(c, err)
+		return
+	}
+	response.OK(c, item)
+}
+
+func (h *DialogHandler) Delete(c *gin.Context) {
+	ownerID, id, ok := h.ownerAndID(c)
+	if !ok {
+		return
+	}
+	if err := h.service.Delete(c.Request.Context(), ownerID, id); err != nil {
+		writeAppError(c, err)
+		return
+	}
+	response.OK(c, gin.H{"success": true})
+}
+
+func (h *DialogHandler) ownerAndID(c *gin.Context) (int64, int64, bool) {
+	ownerID, ok := currentUserID(c)
+	if !ok {
+		response.Error(c, http.StatusUnauthorized, agenterrors.CodeUnauthorized, agenterrors.ErrUnauthorized.Error())
+		return 0, 0, false
+	}
+	id, err := parseInt64Param(c, "id")
+	if err != nil || id <= 0 {
+		writeAppError(c, agenterrors.ErrInvalidInput)
+		return 0, 0, false
+	}
+	return ownerID, id, true
+}
